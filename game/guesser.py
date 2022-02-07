4548466584a87735f6b1cf3280a5721af312725f
roj21008 <roj21008@byui.edu>
2022-02-07 15:53:04 -0500

    jumper

---
+++ game/guesser.py
@@ -0,0 +1,38 @@
+from game.word import Words
+import random
+class Guesser: 
+    
+    def __init__(self):
+        #self.word = Words
+        self.list = ['stock', 'over', 'flow']
+        self.word = random.choice(self.list)
+        self._word_in_progress = []
+        self.j = 0
+        self.size = len(self.word)
+        for self.j in self.word:
+            self._word_in_progress += '_'
+
+    def guess (self): 
+        failed = 0
+        self.guesser = True
+        j = 0
+
+        guess = input('Guess a letter [a-z]: ')
+
+        for letter in self.word:
+                if self._word_in_progress[j] == '_':
+                    if letter == guess:
+                        self._word_in_progress[j] = letter
+                    #replace the j element in the line
+                    else:
+                        failed += 1
+                else:
+                    failed += 1
+                j += 1
+        
+        if failed == self.size:
+            self.guesser == False
+
+
+                
+        return self._word_in_progress
---
+++ game/jumper.py
@@ -0,0 +1,48 @@
+from game.guesser import Guesser
+
+class Jumper:
+    """Updates the status of Jumper, showing the guesser how many guess they have left. 
+    
+    The responsibility of a Jumper is to keep track of many many guesses are left.
+    
+    Attributes:
+        jumper (List['']): Image of jumper
+        trys (int): Number of guesses the user has
+    """
+    
+
+    def __init__(self):
+        """Constructs a new Jumper.
+        Args:
+            self (Jumper): An instance of Jumper.
+        """
+        self._jumper = ['  ___',
+        ' /___\ ',
+        ' \   / ',
+        '  \ / ',
+        '   O  ',
+        '  /|\ ',
+        '  / \ ',
+        '      ',
+        '^^^^^^^']
+        self.trys = 5
+        self._guesser = Guesser()
+
+        
+    def update_jumper(self):
+        """Updates the Jumpers image
+        
+        Returns:
+           self._jumper
+        """
+        if self._guesser is False: 
+            self._jumper.pop(0)
+            self.trys -= 1
+        if self.trys == 0:
+            self._jumper.insert(0,'x')
+            for i in range(len(self._jumper)):
+                print(self._jumper[i])
+            quit()
+        for i in range(len(self._jumper)):
+            print(self._jumper[i])
+        return
