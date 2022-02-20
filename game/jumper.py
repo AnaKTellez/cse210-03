dfcc980234a725a26d4fc9cf68ab0133ff13409f
roj21008 <roj21008@byui.edu>
2022-02-19 23:33:09 -0500

    jumper

---
+++ game/jumper.py
@@ -0,0 +1,57 @@
+from game.Guesser import Guesser
+	
+
+class Jumper:
+    """Updates the status of Jumper, showing the guesser how many guess they have left. 
+    
+    The responsibility of a Jumper is to keep track of many many guesses are left.
+    
+    Attributes:
+        jumper (List['']): Image of jumper
+        trys (int): Number of guesses the user has
+    """
+    
+
+
+    def __init__(self):
+        """Constructs a new Jumper.
+        Args:
+            self (Jumper): An instance of Jumper.
+        """
+        self._jumper = ['  ___',
+                        ' /___\ ',
+                        ' \   / ',
+                        '  \ / ',
+                        '   O  ',
+                        '  /|\ ',
+                        '  / \ ',
+                        '      ',
+                        '^^^^^^^']
+        self.trys = 5
+        self._guesser = Guesser()
+
+
+        
+    def update_jumper(self,guess):
+        """Updates the Jumpers image
+        
+        Returns:
+            self._jumper
+        """
+        if guess == False: 
+            self._jumper.pop(0)
+            self.trys = self.trys - 1
+        if self.trys == 0:
+            self._jumper.insert(0,'   x  ')
+            for i in range(len(self._jumper)):
+                print(self._jumper[i])
+            print('Game Over')
+            quit()
+        for i in range(len(self._jumper)):
+            print(self._jumper[i])
+        return 
+
+
+    def print_jumper(self):
+        for i in range(len(self._jumper)):
+            print(self._jumper[i])
