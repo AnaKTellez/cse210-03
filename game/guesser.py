a45388e66ac06443e84ca7b770798e5427d60ef2
roj21008 <roj21008@byui.edu>
2022-02-19 23:24:02 -0500

    updated

--- game/guesser.py
+++ game/guesser.py
@@ -1,38 +1,49 @@
-from game.word import Words
 import random
 class Guesser: 
+    """A person who guess the letter. 
+    
+    The responsibility of a Guesser is to receive the input letter and compare it with the 
+    random secrer word.
+    Attributes:
+        list (list): List of random words.
+        word (str): secret word 
+        _word_in_progress (list): Guessed letters 
+        j (int): loop index
+        size (int) = lenght of word
+    """
     
     def __init__(self):
-        #self.word = Words
-        self.list = ['stock', 'over', 'flow']
+        """Constructs a new Guesser.
+        
+        Args:
+            self (Guesser): an instance of Guesser.
+        """
+        
+        self.list = ['stock', 'over', 'flow', 'random', 'hello', 'computer', 'happy', 'python', 'health', 'funny']
         self.word = random.choice(self.list)
         self._word_in_progress = []
         self.j = 0
         self.size = len(self.word)
+
         for self.j in self.word:
             self._word_in_progress += '_'
 
-    def guess (self): 
-        failed = 0
-        self.guesser = True
-        j = 0
-
-        guess = input('Guess a letter [a-z]: ')
-
-        for letter in self.word:
-                if self._word_in_progress[j] == '_':
-                    if letter == guess:
-                        self._word_in_progress[j] = letter
-                    #replace the j element in the line
-                    else:
-                        failed += 1
-                else:
-                    failed += 1
-                j += 1
+    def guess (self):  
+        """Gets the guess from the player and compare it with the secret word.
+        Args:
+            self (Guesser): An instance of Guesser.
         
-        if failed == self.size:
-            self.guesser == False
+        Returns:
+            list: word in progress.
+        """
+        self.guesser = False
 
+        guess = input('Enter a letter [a-z]: ')
 
-                
-        return self._word_in_progress
+        for idx, character in enumerate(self.word):
+            if character == guess:
+                self._word_in_progress[idx] = guess
+                self.guesser = True
+                break
+
+        return self._word_in_progress 
